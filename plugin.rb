9ee1c9762fb2568adb2df2f21ab54d200faa0c8a
Keegan George <kgeorge13@gmail.com>
2022-05-10 10:12:22 -0700

    DEV: Add custom fields

--- plugin.rb
+++ plugin.rb
@@ -13,4 +13,55 @@ enabled_site_setting :stemaway_tiered_tagging_enabled
 register_asset 'stylesheets/common.scss'
 
 after_initialize do
+  fields = [
+    { name: 'pathway', type: 'json' },
+    { name: 'skill', type: 'json' },
+    { name: 'sub_skill', type: 'json' },
+    { name: 'other_tags', type: 'json' }
+  ]
+
+  fields.each do |field|
+    # Register the fields
+    register_topic_custom_field_type(field[:name], field[:type].to_sym)
+
+    # Getter Methods
+    add_to_class(:topic, field[:name].to_sym) do
+      if !custom_fields[field[:name]].nil?
+        custom_fields[field[:name]]
+      else
+        nil
+      end
+    end
+
+    # Setter Methods
+    add_to_class(:topic, "#{field[:name]}=") do |value|
+      custom_fields[field[:name]] = value
+    end
+
+    # Update on Topic Creation
+    on(:topic_created) do |topic, opts, user|
+      topic.send("#{field[:name]}=".to_sym, opts[field[:name].to_sym])
+      topic.save!
+    end
+
+    # Update on Topic Edit
+    PostRevisor.track_topic_field(field[:name].to_sym) do |tc, value|
+      tc.record_change(field[:name], tc.topic.send(field[:name]), value)
+      tc.topic.send("#{field[:name]}=".to_sym, value.present? ? value : nil)
+    end
+
+    # Serialize to Topic
+    add_to_serializer(:topic_view, field[:name].to_sym) do
+      object.topic.send(field[:name])
+    end
+
+    # Preload the Fields
+    add_preloaded_topic_list_custom_fields(field[:name])
+
+    # Serialize to the topic list
+    add_to_serializer(:topic_list_item, field[:name].to_sym) do
+      object.send(field[:name])
+    end
+  end
+
 end
